65739a7a57ffd734a3741360795ded5ee697d3c6
f-gouya <farzad.gouya@live.com>
2024-08-29 22:31:54 +0330

    First_Edition_Home

---
+++ PresentationLayer/main_view.py
@@ -0,0 +1,22 @@
+from PresentationLayer.windows import Windows
+# from PresentationLayer.home import HomeFrame
+
+
+class MainView:
+    def __init__(self):
+        self.window = Windows()
+
+        self.frames = {}
+
+        # self.add_frame("home", HomeFrame(self.window, self))
+
+        self.window.show_form()
+
+    def add_frame(self, name, frame):
+        self.frames[name] = frame
+        self.frames[name].grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+    def switch_frame(self, frame_name):
+        frame = self.frames[frame_name]
+        frame.tkraise()
+        return frame
---
+++ PresentationLayer/windows.py
@@ -0,0 +1,16 @@
+from ttkbootstrap import Window
+
+
+class Windows(Window):
+    def __init__(self):
+        super().__init__(themename="flatly")
+
+        # self.geometry("800x500")
+        self.title("Task Manager Application")
+        # self.resizable(False, False)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+    def show_form(self):
+        self.mainloop()
